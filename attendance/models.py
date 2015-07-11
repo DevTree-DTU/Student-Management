446fa46c2e5850f8a77b74312abff516eb3a4a84
Feroz Ahmad <fz29@localhost.localdomain>
2015-07-12 00:09:27 +0530

    attendance model-index added

--- attendance/models.py
+++ attendance/models.py
@@ -4,9 +4,13 @@ from login.models import Teacher, Student,Batch
 
 # Create your models here.
 class attendance(models.Model):
-    studentId = models.ForeignKey(Student)
-    batchId = models.ForeignKey(Batch)
+    studentId = models.ForeignKey(Student,db_index=True)  #indexing is on for studentId
+    batchId = models.ForeignKey(Batch,db_index=True)  #indexing is on for batchId
     subjectId= models.ForeignKey(Subject)
     teacherId = models.ForeignKey(Teacher)
+    date=models.DateField()
     #time=models.DateField()
     #attendanceWeight=models.BigIntegerField()
+
+     class Meta:
+        index_together = [['batchId', 'studentId']]

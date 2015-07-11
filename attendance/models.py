7d23716f299407293282566864f32077de723729
Feroz Ahmad <fz29@localhost.localdomain>
2015-07-11 19:09:01 +0530

    attendance model created

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from login.models import Teacher, Student,Batch
+#import subjects
 
 # Create your models here.
+class attendance(models.Model):
+    studentId = models.ForeignKey(Student)
+    batchId = models.ForeignKey(Batch)
+    subjectId= models.ForeignKey(Subject)
+    teacherId = models.ForeignKey(Teacher)
+    #time=models.DateField()
+    #attendanceWeight=models.BigIntegerField()

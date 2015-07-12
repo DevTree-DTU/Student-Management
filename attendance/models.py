9feea8e516d94aa2bb4b62684ea7070bcbcd2eef
Feroz Ahmad <fz29@localhost.localdomain>
2015-07-12 12:57:23 +0530

    Attendance API skeleton + changes in Attendance model

--- attendance/models.py
+++ attendance/models.py
@@ -5,12 +5,13 @@ from login.models import Teacher, Student,Batch
 # Create your models here.
 class attendance(models.Model):
     studentId = models.ForeignKey(Student,db_index=True)  #indexing is on for studentId
-    batchId = models.ForeignKey(Batch,db_index=True)  #indexing is on for batchId
+    batchId = models.ForeignKey()  #indexing is on for batchId
     subjectId= models.ForeignKey(Subject)
     teacherId = models.ForeignKey(Teacher)
     date=models.DateField()
-    #time=models.DateField()
-    #attendanceWeight=models.BigIntegerField()
+    presentPoints=models.IntegerField()
+    attendanceWeight=models.IntegerField()
+
 
      class Meta:
-        index_together = [['batchId', 'studentId']]
+        index_together = [['batchId', 'studentId']]  #useful for editing the attendance
--- attendance/views.py
+++ attendance/views.py
@@ -1,2 +1,37 @@
 from django.shortcuts import render
+import datetime
+def percentageCalc(listOfRollCalls):
+    totalCalls=0
+    presentCalls=0
+    recentPresentCalls=0  #stores last complete months attendance.
+    recentTotalCalls=0
 
+    previousMonth=datetime.date.today().month - 1
+
+    for rollCall in listOfRollCalls
+        totalCalls+=rollCall.attendanceWeight
+        presentCalls+=rollCall.presentPoints
+        if rollCall.date.month==previousMonth:
+            recentTotalCalls+=rollCall.attendanceWeight
+            recentPresentCalls+=rollCall.presentPoints
+    return (recentPresentCalls*100.0/recentTotalCalls,presentCalls*100.0/totalCalls)
+
+
+def studentPercentage(request):
+    #student_id=
+    allRollCalls=attendance.objects.filter(studentId=student_id)
+    percentages=percentageCalc(allRollCalls)
+    return {'monthlyPercentage':percentages[0],'TotalPercentage':percentages[1] }
+
+def rollCallForBatch(request)
+    #selectBatch
+    #loadstudents
+    #send student names
+    #get present/absent/attendance
+    #save the records
+
+def editAttendanceForBatch()
+    #selectBatch
+    #selectSubject
+    #fetch all roll calls
+    #send the json

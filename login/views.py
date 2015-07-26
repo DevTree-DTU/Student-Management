79bc4888dc928f56b15f75fc3b344a59d48126fb
Arpit Gogia <earpitg@gmail.com>
2015-07-26 22:05:58 +0530

    All responses now in JSON

--- login/views.py
+++ login/views.py
@@ -1,45 +1,85 @@
 from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from login.models import Student, Teacher
+import json
 def saveStudent(request):
-    s = Student()
-    s.username = request.POST['Username']
-    s.password = request.POST['Password']
-    s.name = request.POST['Name']
-    s.address = request.POST['Address']
-    s.phoneNo = request.POST['Phone']
-    s.fathersName = request.POST['Fathers Name']
-    s.DOB = request.POST['DOB']
-    s.branch = request.POST['Branch']
-    s.admissionYear = request.POST['Year']
-    s.email = request.POST['Email']
-    s.isCR = 'False'
-    s.studentID = s.username
-    s.batchID = 'A6'
-    s.save()
-    response = HttpResponse("Student Saved")
+    try:
+        s = Student()
+        s.username = request.POST['Username']
+        s.password = request.POST['Password']
+        s.name = request.POST['Name']
+        s.address = request.POST['Address']
+        s.phoneNo = request.POST['Phone']
+        s.fathersName = request.POST['FathersName']
+        s.DOB = request.POST['DOB']
+        s.branch = request.POST['Branch']
+        s.admissionYear = request.POST['Year']
+        s.email = request.POST['Email']
+        s.isCR = 'False'
+        s.studentID = s.username
+        s.batchID = 'A6'
+        s.save()
+    except Exception as e:
+        #responseArray = []
+        data = {}
+        data['Status'] = 0
+        data['Description'] = str(e)
+        #responseArray.append(data)
+        response = HttpResponse(json.dumps(data), content_type="application/json")
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Max-Age"] = "1000"
+        response["Access-Control-Allow-Headers"] = "*"
+        return response
+    #responseArray = []
+    data = {}
+    data['Status'] = int(1)
+    data['Description'] = 'Registration Successful'
+    #responseArray.append(data)
+    response = HttpResponse(json.dumps(data), content_type="application/json")
     response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
     return response
-
+#    response = HttpResponse("Student Saved")
+#    response["Access-Control-Allow-Origin"] = "*"
 def saveTeacher(request):
-    t = Teacher()
-    s.username = request.POST['Username']
-    s.password = request.POST['Password']
-    t.name = request.POST['Name']
-    t.address = request.POST['Address']
-    t.phoneNo = request.POST['Phone']
-    t.department = request.POST['Department']
-    t.roomNo = request.POST['Room']
-    t.designation = request.POST['Designation']
-    t.email = request.POST['Email']
-    t.dateOfJoining = request.POST['DOJ']
-    t.isHOD = request.POST['HOD']
-    t.expertise = request.POST['Expertise']
-    t.save()
-    response = HttpResponse("Teacher Saved")
-    response["Access-Control-Allow-Origin"] = "*"
-    return response
+    try:
+        t = Teacher()
+        t.username = request.POST['Username']
+        t.password = request.POST['Password']
+        t.name = request.POST['Name']
+        t.address = request.POST['Address']
+        t.department = request.POST['Department']
+        t.phoneNo = request.POST['Phone']
+        t.roomNo = request.POST['Room']
+        t.designation = request.POST['Designation']
+        t.email = request.POST['Email']
+        t.dateOfJoining = request.POST['DOJ']
+        t.isHOD = request.POST['HOD']
+        t.expertise = request.POST['Expertise']
+        t.save()
+    except Exception as e:
+        #responseArray = []
+        data = {}
+        data['Status'] = 0
+        data['Description'] = str(e)
+        #responseArray.append(data)
+        response = HttpResponse(json.dumps(data), content_type="application/json")
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Max-Age"] = "1000"
+        response["Access-Control-Allow-Headers"] = "*"
+        return response
+    #responseArray = []
+    data = {}
+    data['Status'] = int(1)
+    data['Description'] = 'Registration Successful'
+    #responseArray.append(data)
+    response = HttpResponse(json.dumps(data), content_type="application/json")
 
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 def login_user(request):
     username = password = ''
     if request.POST:
@@ -49,14 +89,34 @@ def login_user(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                response = HttpResponse('Login Successful')
+                data = {}
+                data['Status'] = int(1)
+                data['Description'] = 'Login Successful'
+                response = HttpResponse(json.dumps(data), content_type="application/json")
                 response["Access-Control-Allow-Origin"] = "*"
+                response["Access-Control-Allow-Headers"] = "*"
                 return response
             else:
-                response = HttpResponse('Login Unsuccessful')
+                data = {}
+                data['Status'] = int(0)
+                data['Description'] = 'Login Unsuccessful'
+                response = HttpResponse(json.dumps(data), content_type="application/json")
                 response["Access-Control-Allow-Origin"] = "*"
+                response["Access-Control-Allow-Headers"] = "*"
                 return response
         else:
-            response = HttpResponse('Username not found')
+            data = {}
+            data['Status'] = int(0)
+            data['Description'] = 'Username not found'
+            response = HttpResponse(json.dumps(data), content_type="application/json")
             response["Access-Control-Allow-Origin"] = "*"
+            response["Access-Control-Allow-Headers"] = "*"
             return response
+    else:
+        data = {}
+        data['Status'] = int(0)
+        data['Description'] = 'POST Requests only'
+        response = HttpResponse(json.dumps(data), content_type="application/json")
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Headers"] = "*"
+        return response

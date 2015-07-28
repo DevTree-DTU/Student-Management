b25ffac4da89415916ce924cae16d78a129bd841
Feroz Ahmad <fz29@localhost.localdomain>
2015-07-28 12:34:18 +0530

    date_Wise and week_wise done

--- attendance/views.py
+++ attendance/views.py
@@ -27,6 +27,7 @@ def home(request):
             lastMonthRollCalls=Attendance.objects.filter(student_ID= student_ID, subject_ID=subject.id, timestamp.month = previousMonth)
             percentOverall=percentageRoll(allRollCalls)
             percentLastMonth=percentageRoll(lastMonthRollCalls)
+
             #adding details in dictionary
             subWisePercent['subcode']=Subject.subjectCode
             subWisePercent['subname']=Subject.subjectName
@@ -37,7 +38,7 @@ def home(request):
 
     #elif teacher
 
-        #to show teachers time table. Send whic classes attendance is done and whose is left.
+        #to show teachers time table. Send which classes attendance is done and whose is left.
 
     response = HttpResponse(json.dumps(responseArray), content_type="application/json")
     response["Access-Control-Allow-Origin"] = "*"
@@ -51,7 +52,7 @@ def week_wise(request):
     day_of_week=datetime.datetime.now().isocalendar()[2] #Monday=0, Sunday=7 #gives today's day
     date_today=datetime.datetime.now()  #today's date
     date_start=date_today-datetime.timedelta(days=day_of_week)  #starting of the week
-    date_end=date_start + datetime.timedelta(days=7) #
+    date_end=date_start + datetime.timedelta(days=7)  #week end
 
     studentbatch_ID=Student.batch_ID  #find students batch id
     responseArray=[]
@@ -76,12 +77,69 @@ def week_wise(request):
 
 
 @login_required
-def month_wise(request):
-    pass
+def month_wise(request):  #LEFT
+    date_today=datetime.datetime.now()
+
+    if date_today.date().month >= projparams.SEM1_START_MONTH and date_today.date().month < projparams.SEM2_START_MONTH:
+        startMonthPoint = date_today.replace(month=projparams.SEM1_START_MONTH,day=1,hour=0, minute=0)
+    else:
+        startMonthPoint = date_today.replace(month=projparams.SEM2_START_MONTH,day=1,hour=0, minute=0)
+
+    studentbatch_ID=Student.batch_ID  #find students batch id
+    responseArray=[]
+
+
+        for subject in Subjects.objects.filter(batch_ID=studentbatch_ID):
+            subWisePercent={}
+            rollCallWeek=Attendance.objects.filter(student_ID=student_ID, subject_ID=subject.id, updated__gt=startMonthPoint)
+            perc=percentageRoll(rollCallWeek)
+            #adding details in dictionary
+            subWisePercent['subcode']=Subject.subjectCode
+            subWisePercent['subname']=Subject.subjectName
+            subWisePercent['weekPercentage']=perc
+            responseArray.append(subWisePercent)
+
+    response = HttpResponse(json.dumps(responseArray), content_type="application/json")
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
 
 @login_required
 def date_wise(request):
-    pass
+    #collecting form data
+    if ('date' in request.GET) and ('month' in request.GET) and ('year' in request.GET) and request.GET['date'] and request.GET['month'] and request.GET['year']  :
+        date_form = request.GET['date']
+        month_form  = request.GET['month']
+        year_form = request.GET['year']
+
+    else:
+        return render(request, 'same.html', {'error': True})
+
+    date_today=datetime.datetime.now()
+    day_start =  datetime.datetime( month_form, date_form, year_form, 00 , 01 , 00)  #(Month, Day, Year, Hour, Minute, second)
+    day_end =  datetime.datetime datetime.datetime( month_form, date_form, year_form, 23 , 59 , 59)
+
+    studentbatch_ID=Student.batch_ID  #find students batch id
+    responseArray=[]
+    for subject in Subjects.objects.filter(batch_ID=studentbatch_ID).order_by(subject_name):
+        subWisePercent={}
+        rollCallWeek=Attendance.objects.filter(student_ID=student_ID, subject_ID=subject.id, updated__lte=day_end, updated__gt=day_start)
+        perc=percentageRoll(rollCallWeek)
+        #adding details in dictionary
+        subWisePercent['subcode']=Subject.subjectCode
+        subWisePercent['subname']=Subject.subjectName
+        subWisePercent['weekPercentage']=perc
+        responseArray.append(subWisePercent)
+
+    response = HttpResponse(json.dumps(responseArray), content_type="application/json")
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
+
 
 @login_required
 def subjectPage(request):
